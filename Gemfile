307c96434575c8a719cc2975f09b90761c63db9a
ZahBenjamin <bz@benzah.com>
2023-12-20 12:22:27 -0500

    Phase 1 MVP

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source 'https://rubygems.org'
+
+gem 'nokogiri'
+gem 'rack'
+gem 'rspec'
+gem 'discordrb'
+gem 'dotenv-rails', groups: [:development, :test]
+gem 'httparty'
--- app/bot.rb
+++ app/bot.rb
@@ -1,11 +1,42 @@
 # frozen_string_literal: true
 
 require 'discordrb'
-require 'config/secrets.yml'
+require 'dotenv'
+# require 'net/http'
+require 'httparty'
+Dotenv.load
 
-@my_bot = Discordrb::Bot.new token: @bot_token, client_id: @client_id
-@my_bot.run true
+
+
+
+bot = Discordrb::Commands::CommandBot.new token: ENV['BOT_TOKEN'], prefix:'!'
+bot.run true
 
 # commands go here
 
-@my_bot.join
+bot.command :get_request do |event|
+
+uri = URI('http://ratingupdate.info/api/player_rating/2EC3EA2818411DB/TE') 
+response = HTTParty.get(uri)
+
+  if response.success?
+    data = JSON.parse(response.body)
+    puts "API Data: #{data}"
+
+    event.respond "MMR: #{data["value"]}, DEVIATION: #{data["deviation"]}"
+  else
+    puts "Error: #{response.code}"
+    event.respond "Error: #{response.code}"
+  end
+end
+
+
+
+bot.command(:invite, chain_usable: false) do |event|
+  # This simply sends the bot's invite URL, without any specific permissions,
+  # to the channel.
+  event.bot.invite_url
+end
+
+
+bot.join
