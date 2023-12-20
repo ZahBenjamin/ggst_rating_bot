e701f5fe3360336385f53cae8b07b27bd8c391f8
ZahBenjamin <bz@benzah.com>
2023-12-20 12:23:47 -0500

    Remove token

--- ping.rb
+++ ping.rb
@@ -13,7 +13,7 @@ require 'discordrb'
 # you, look here: https://github.com/discordrb/discordrb/wiki/Redirect-URIs-and-RPC-origins
 # After creating the bot, simply copy the token (*not* the OAuth2 secret) and put it into the
 # respective place.
-bot = Discordrb::Bot.new token: 'MTE1OTMwNjI0NTIwMzgyMDYyNg.GxFtos.QQJGRMgoUvR6IM3U_n6w6hqrmITLRNKQKwdtLg'
+bot = Discordrb::Bot.new token: 
 
 # Here we output the invite URL to the console so the bot account can be invited to the channel. This only has to be
 # done once, afterwards, you can remove this part if you want
