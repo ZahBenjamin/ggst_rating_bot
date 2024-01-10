b271ded22e65d3845cd4517ef66450698f4a4bfe
ZahBenjamin <bz@benzah.com>
2024-01-10 13:51:46 -0500

    MVP finishes

--- app/bot.rb
+++ app/bot.rb
@@ -7,14 +7,14 @@ require 'httparty'
 Dotenv.load
 
 
-
-
+# Start bot
 bot = Discordrb::Commands::CommandBot.new token: ENV['BOT_TOKEN'], prefix:'!'
 bot.run true
 
+
 # commands go here
 
-bot.command :get_rating do |event, account_id, character_tag|
+bot.command(:rating, description: 'Lorum ipsum') do |event, account_id, character_tag|
 
 uri = URI('http://ratingupdate.info/api/player_rating/' + account_id + '/' + character_tag) 
 response = HTTParty.get(uri)
@@ -23,7 +23,7 @@ response = HTTParty.get(uri)
     data = JSON.parse(response.body)
     puts "API Data: #{data}"
 
-    event.respond "MMR: #{data["value"].round} DEVIATION: #{data["deviation"].round}"
+    event.respond "MMR: #{data["value"].round} ± #{data["deviation"].round}"
   else
     puts "Error: #{response.code}"
     event.respond "Error: #{response.code}"
@@ -31,8 +31,22 @@ response = HTTParty.get(uri)
 end
 
 
+# text based commands
 
-bot.command(:invite, chain_usable: false) do |event|
+bot.command :help do |event|
+  event << 'Type !rating   <account_id>   <character_tag>  to get your MMR on ratingupdate!'
+  event << 'Type !invite to get an invite code for the discord bot'
+  event << 'Find account id and character by searching for your user on ratingupdate.info'
+  event << 'After you find the right account_id and character code, try leaving a note on the bot profile on the right'
+end
+
+bot.command :tags do |event|
+  event << 'SO  ==  Sol'
+end
+
+
+
+bot.command :invite do |event|
   # This simply sends the bot's invite URL, without any specific permissions,
   # to the channel.
   event.bot.invite_url
